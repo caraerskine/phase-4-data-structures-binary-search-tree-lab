b044b672fd333d22b94291c1e3704e75dc6e56ec
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-07-24 10:47:42 -0400

    commit tree b141c85560e5ebff22cb0c3cffcb96c9a69fb366

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -7,12 +7,50 @@ class BinarySearchTree
     @root = root
   end
 
-  def search(value)
-    # your code here
+  def search(target_value)
+    current = @root
+
+    while !current.nil?
+      if target_value == current.value
+        return current
+      elsif target_value < current.value
+        current = current.left
+      else
+        current = current.right
+      end
+    end
+
+    return nil
   end
 
   def insert(value)
-    # your code here
-  end
+    new_node = Node.new(value)
 
+    if @root.nil?
+      @root = new_node
+      return new_node
+    end
+
+    current = @root
+
+    loop do
+      if value == current.value
+        return nil # Don't allow duplicate nodes
+      elsif value < current.value
+        if current.left.nil?
+          current.left = new_node
+          return new_node
+        else
+          current = current.left
+        end
+      else
+        if current.right.nil?
+          current.right = new_node
+          return new_node
+        else
+          current = current.right
+        end
+      end
+    end
+  end
 end 
